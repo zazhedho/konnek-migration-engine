b7746a0d14d59dcbc5802034f763caf86cf1e0b8
nasrulqodri <nasrul.qodri@sprintasia.co.id>
2023-12-02 10:16:54 +0700

    fetch data

--- unavailable_reasons.go
+++ unavailable_reasons.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/joho/godotenv/autoload"
+	uuid "github.com/satori/go.uuid"
+	"konnek-migration/models"
 	"konnek-migration/utils"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -26,4 +31,54 @@ func main() {
 			utils.WriteLog(fmt.Sprintf("Close Connection scDb; ERROR: %+v", err), utils.LogLevelError)
 		}
 	}(dstDB)
+
+	logID := uuid.NewV4()
+	logPrefix := fmt.Sprintf("[%v] [unavailable_reason]", logID)
+	utils.WriteLog(fmt.Sprintf("%s start...", logPrefix), utils.LogLevelDebug)
+
+	// Migrate
+	tStart := time.Now()
+	debug := 0
+	debugT := time.Now()
+
+	if os.Getenv("START_DATE") != "" && os.Getenv("END_DATE") != "" {
+		scDB.Where("created_at BETWEEN ? AND ?", os.Getenv("START_DATE"), os.Getenv("END_DATE"))
+	} else if os.Getenv("START_DATE") != "" && os.Getenv("END_DATE") == "" {
+		scDB.Where("created_at >=?", os.Getenv("START_DATE"))
+	} else if os.Getenv("START_DATE") == "" && os.Getenv("END_DATE") != "" {
+		scDB.Where("created_at <=?", os.Getenv("END_DATE"))
+	}
+
+	if os.Getenv("ORDER_BY") != "" {
+		sortMap := map[string]string{
+			"created_at": "created_at",
+		}
+		if strings.ToUpper(os.Getenv("ORDER_DIRECTION")) == "DESC" {
+			scDB = scDB.Order(sortMap[os.Getenv("ORDER_BY")] + " DESC")
+		} else {
+			scDB = scDB.Order(sortMap[os.Getenv("ORDER_BY")])
+		}
+	}
+
+	offset, _ := strconv.Atoi(os.Getenv("OFFSET"))
+	limit, _ := strconv.Atoi(os.Getenv("LIMIT"))
+
+	if offset > 0 {
+		scDB = scDB.Offset(offset)
+	}
+
+	if limit > 0 {
+		scDB = scDB.Limit(limit)
+	}
+
+	var list []models.MtrReasonAvailability
+	if err := scDB.Find(&list).Error; err != nil {
+		utils.WriteLog(fmt.Sprintf("%s; fetch error: %v", logPrefix, err), utils.LogLevelError)
+		return
+	}
+	debug++
+	utils.WriteLog(fmt.Sprintf("%s [FETCH] DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+	debugT = time.Now()
+
+	utils.WriteLog(fmt.Sprintf("%s end; duration: %v", logPrefix, time.Now().Sub(tStart)), utils.LogLevelDebug)
 }
