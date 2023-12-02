c6ecb98732b525e09eb2b52cf2b390b8b9387255
Dimas Taufikurrisyad <dimas.taufikurrisyad@sprintasia.co.id>
2023-12-02 20:30:08 +0700

    Engine fetch company configs fetch data existing

---
+++ company_configs.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+	"konnek-migration/models"
+	"konnek-migration/utils"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// Create source DB connection
+	scDB := utils.GetDBConnection()
+	defer func(scDB *gorm.DB) {
+		err := scDB.Close()
+		if err != nil {
+			utils.WriteLog(fmt.Sprintf("Close Connection scDb; ERROR: %+v", err), utils.LogLevelError)
+		}
+	}(scDB)
+
+	// Create destination DB connection
+	dstDB := utils.GetDBNewConnection()
+	defer func(dstDB *gorm.DB) {
+		err := dstDB.Close()
+		if err != nil {
+			utils.WriteLog(fmt.Sprintf("Close Connection scDb; ERROR: %+v", err), utils.LogLevelError)
+		}
+	}(dstDB)
+
+	logID := uuid.NewV4()
+	logPrefix := fmt.Sprintf("[%v] [Companies Configs]", logID)
+	utils.WriteLog(fmt.Sprintf("%s start...", logPrefix), utils.LogLevelDebug)
+
+	tStart := time.Now()
+	debug := 0
+	debugT := time.Now()
+
+	var comConfSc []models.Configuration
+
+	if os.Getenv("COMPANYID") != "" {
+		scDB = scDB.Where("company_id = ?", os.Getenv("COMPANYID"))
+	}
+
+	//Fetch companies existing
+	if err := scDB.Find(&comConfSc).Error; err != nil {
+		utils.WriteLog(fmt.Sprintf("%s; fetch error: %v", logPrefix, err), utils.LogLevelError)
+		return
+	}
+
+	totalCompany := len(comConfSc)
+
+	debug++
+	utils.WriteLog(fmt.Sprintf("%s [FETCH] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, totalCompany, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+	debugT = time.Now()
+
+	insertedCount := 0
+	successCount := 0
+	errorCount := 0
+	log.Printf("DATA COMPANY CONFIG : %v", comConfSc)
+	//var errorMessages []string
+
+	//for _, comConf := range comConfSc {
+	//	var comConfDst models.CompanyConfig
+	//
+	//
+	//
+	//	insertedCount++
+	//	reiInsertCount := 0
+	//reInsert:
+	//	if err := dstDB.Create(&comConfDst).Error; err != nil {
+	//		if errCode, ok := err.(*pq.Error); ok {
+	//			if errCode.Code == "23505" { //unique_violation
+	//				reiInsertCount++
+	//				comConfDst.Id = uuid.NewV4()
+	//				if reiInsertCount < 3 {
+	//					goto reInsert
+	//				}
+	//			}
+	//		}
+	//		utils.WriteLog(fmt.Sprintf("%s; [INSERT] Error: %v", logPrefix, err), utils.LogLevelError)
+	//		errorCount++
+	//		errorMessages = append(errorMessages, fmt.Sprintf("%s [INSERT] Error: %v ; DATA: %v", time.Now(), err, comConfDst))
+	//		continue
+	//	}
+	//
+	//	successCount++
+	//}
+	//
+	//// Write error messages to a text file
+	//formattedTime := time.Now().Format("2006-01-02_150405")
+	//errorFileLog := fmt.Sprintf("error_messages_companies_%s.log", formattedTime)
+	//if len(errorMessages) > 0 {
+	//	createFile, errCreate := os.Create(errorFileLog)
+	//	if errCreate != nil {
+	//		utils.WriteLog(fmt.Sprintf("%s [ERROR] Create File Error Log; Error: %v;", logPrefix, errCreate), utils.LogLevelError)
+	//		return
+	//	}
+	//	defer createFile.Close()
+	//
+	//	for _, errMsg := range errorMessages {
+	//		_, errWrite := createFile.WriteString(errMsg + "\n")
+	//		if errWrite != nil {
+	//			utils.WriteLog(fmt.Sprintf("%s [ERROR] Write File Error Log; Filename: %s; Error: %v;", logPrefix, errorFileLog, errCreate), utils.LogLevelError)
+	//		}
+	//	}
+	//
+	//	utils.WriteLog(fmt.Sprintf("%s [ERROR] Error messages written to %s", logPrefix, errorFileLog), utils.LogLevelError)
+	//}
+
+	debug++
+	utils.WriteLog(fmt.Sprintf("%s [INSERT] TOTAL_INSERTED: %d; TOTAL_SUCCESS: %d; TOTAL_ERROR: %v DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, insertedCount, successCount, errorCount, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+
+	utils.WriteLog(fmt.Sprintf("%s end; duration: %v", logPrefix, time.Now().Sub(tStart)), utils.LogLevelDebug)
+}
---
+++ models/company_configs.go
@@ -0,0 +1,107 @@
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+func (Configuration) TableName() string {
+	return "configurations"
+}
+
+type Configuration struct {
+	Id                   uuid.UUID  `json:"id" gorm:"column:id"`
+	ChatLimit            int        `json:"limit_chat" gorm:"column:limit_chat"`
+	SetLimitChat         bool       `json:"set_limit_chat" gorm:"column:set_limit_chat"`
+	CompanyId            uuid.UUID  `json:"company_id" gorm:"column:company_id"`
+	Bot                  bool       `json:"bot" gorm:"column:bot"`
+	Whatsapp             bool       `json:"whatsapp" gorm:"column:whatsapp"`
+	Line                 bool       `json:"line" gorm:"column:line"`
+	AutoAssign           bool       `json:"auto_assign" gorm:"column:auto_assign"`
+	Telegram             bool       `json:"telegram" gorm:"column:telegram"`
+	FacebookMessenger    bool       `json:"facebook_messenger" gorm:"column:facebook_messenger"`
+	FlagGreeting         bool       `json:"flag_greeting" gorm:"column:flag_greeting"`
+	Greeting             string     `json:"greeting" gorm:"column:greeting"`
+	GreetingOptionsFlag  bool       `json:"greeting_options_flag" gorm:"column:greeting_options_flag"`
+	GreetingOptions      string     `json:"greeting_options" gorm:"column:greeting_options"`
+	WaitingGreetingFlag  bool       `json:"waiting_greeting_flag" gorm:"column:waiting_greeting_flag"`
+	WaitingGreeting      string     `json:"waiting_greeting" gorm:"column:waiting_greeting"`
+	AssignedGreetingFlag bool       `json:"assigned_greeting_flag" gorm:"column:assigned_greeting_flag"`
+	AssignedGreeting     string     `json:"assigned_greeting" gorm:"column:assigned_greeting"`
+	ClosingGreetingFlag  bool       `json:"closing_greeting_flag" gorm:"column:closing_greeting_flag"`
+	ClosingGreeting      string     `json:"closing_greeting" gorm:"column:closing_greeting"`
+	SlaFrom              string     `json:"sla_from" gorm:"column:sla_from"`
+	SlaTo                string     `json:"sla_to" gorm:"column:sla_to"`
+	SlaThreshold         int        `json:"sla_threshold" gorm:"column:sla_threshold"`
+	CreatedAt            time.Time  `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt            time.Time  `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt            *time.Time `json:"-" gorm:"column:deleted_at"`
+	CsatFlag             bool       `json:"csat_flag" gorm:"column:csat_flag"`
+	ReasonFlag           bool       `json:"reason_flag" gorm:"column:reason_flag"`
+	KeywordFilterStatus  bool       `json:"keyword_filter_status" gorm:"column:keyword_filter_status"`
+	KeywordFilter        string     `json:"keyword_filter" gorm:"column:keyword_filter"`
+	KeywordGreetings     string     `json:"keyword_greetings" gorm:"column:keyword_greetings"`
+	UrlWebhook           string     `json:"url_webhook" gorm:"column:url_webhook"`
+	SdkWhatsapp          string     `json:"sdk_whatsapp" gorm:"sdk_whatsapp"`
+	InquirySandeza       bool       `json:"inquiry_sandeza" gorm:"inquiry_sandeza"`
+}
+
+func (CompanyConfig) TableName() string {
+	return "company_configs"
+}
+
+type CompanyConfig struct {
+	Id                        uuid.UUID  `json:"id" gorm:"column:id"`
+	CompanyId                 uuid.UUID  `json:"company_id" gorm:"column:company_id"`
+	Bot                       bool       `json:"bot" gorm:"column:bot"`
+	Whatsapp                  bool       `json:"whatsapp" gorm:"column:whatsapp"`
+	Line                      bool       `json:"line" gorm:"column:line"`
+	Telegram                  bool       `json:"telegram" gorm:"column:telegram"`
+	Facebook                  bool       `json:"facebook" gorm:"column:facebook"`
+	WebWidget                 bool       `json:"web_widget" gorm:"column:web_widget"`
+	AppWidget                 bool       `json:"app_widget" gorm:"column:app_widget"`
+	SdkBot                    string     `json:"sdk_bot" gorm:"column:sdk_bot"`
+	SdkWhatsapp               string     `json:"sdk_whatsapp" gorm:"column:sdk_whatsapp"`
+	SdkLine                   string     `json:"sdk_line" gorm:"column:sdk_line"`
+	SdkTelegram               string     `json:"sdk_telegram" gorm:"column:sdk_telegram"`
+	SdkFacebook               string     `json:"sdk_facebook" gorm:"column:sdk_facebook"`
+	AutoAssign                bool       `json:"auto_assign" gorm:"column:auto_assign"`
+	ChatLimit                 int        `json:"chat_limit" gorm:"column:chat_limit"`
+	SlaFrom                   string     `json:"sla_from" gorm:"column:sla_from"`
+	SlaTo                     string     `json:"sla_to" gorm:"column:sla_to"`
+	SlaThreshold              int        `json:"sla_threshold" gorm:"column:sla_threshold"`
+	WelcomeGreeting           string     `json:"welcome_greeting" gorm:"column:welcome_greeting"`
+	WelcomeGreetingFlag       bool       `json:"welcome_greeting_flag" gorm:"column:welcome_greeting_flag"`
+	WelcomeGreetingOption     string     `json:"welcome_greeting_option" gorm:"column:welcome_greeting_options"`
+	WelcomeGreetingOptionFlag bool       `json:"welcome_greeting_option_flag" gorm:"column:welcome_greeting_options_flag"`
+	WaitingGreeting           string     `json:"waiting_greeting" gorm:"column:waiting_greeting"`
+	WaitingGreetingFlag       bool       `json:"waiting_greeting_flag" gorm:"column:waiting_greeting_flag"`
+	AssignedGreeting          string     `json:"assigned_greeting" gorm:"column:assigned_greeting"`
+	AssignedGreetingFlag      bool       `json:"assigned_greeting_flag" gorm:"column:assigned_greeting_flag"`
+	ClosingGreeting           string     `json:"closing_greeting" gorm:"column:closing_greeting"`
+	ClosingGreetingFlag       bool       `json:"closing_greeting_flag" gorm:"column:closing_greeting_flag"`
+	CsatFlag                  bool       `json:"csat_flag" gorm:"column:csat_flag"`
+	UnavailableReasonFlag     bool       `json:"unavailable_reason_flag" gorm:"column:unavailable_reason_flag"`
+	InquirySandeza            bool       `json:"inquiry_sandeza" gorm:"column:inquiry_sandeza"`
+	Blacklist                 string     `json:"blacklist" gorm:"column:blacklist"`
+	KeywordFilterStatus       bool       `json:"keyword_filter_status" gorm:"column:keyword_filter_status"`
+	KeywordFilter             string     `json:"keyword_filter" gorm:"column:keyword_filter"`
+	KeywordGreetings          string     `json:"keyword_greetings" gorm:"column:keyword_greetings"`
+	MaintenanceStatus         bool       `json:"maintenance_status" gorm:"column:maintenance_status"`
+	MaintenanceMessage        string     `json:"maintenance_message" gorm:"column:maintenance_message"`
+	AutoAssignPersession      bool       `json:"auto_assign_persession" gorm:"column:auto_assign_persession"`
+	SpamMaxChat               int64      `json:"spam_max_chat" gorm:"column:spam_max_chat"`
+	SpamIntervalChat          int        `json:"spam_interval_chat" gorm:"column:spam_interval_chat"`   //in seconds
+	SpamBlockDuration         int        `json:"spam_block_duration" gorm:"column:spam_block_duration"` //in minutes
+	SpamPerContentType        bool       `json:"spam_per_content_type" gorm:"column:spam_per_content_type"`
+	SpamMessage               string     `json:"spam_message" gorm:"column:spam_message"`
+	KeywordMaxInvalid         int64      `json:"keyword_max_invalid" gorm:"column:keyword_max_invalid"`
+	KeywordInterval           int        `json:"keyword_interval" gorm:"column:keyword_interval"`             //in seconds
+	KeywordBlockDuration      int        `json:"keyword_block_duration" gorm:"column:keyword_block_duration"` //in minutes
+	CreatedAt                 time.Time  `json:"created_at" gorm:"column:created_at"`
+	CreatedBy                 uuid.UUID  `json:"created_by" gorm:"column:created_by"`
+	UpdatedAt                 time.Time  `json:"updated_at" gorm:"column:updated_at"`
+	UpdatedBy                 uuid.UUID  `json:"updated_by" gorm:"column:updated_by"`
+	DeletedAt                 *time.Time `json:"-" gorm:"column:deleted_at"`
+	DeletedBy                 uuid.UUID  `json:"deleted_by" gorm:"column:deleted_by"`
+}
