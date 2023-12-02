c7975147c293d87b3a2be091c663fddca905b545
nasrulqodri <nasrul.qodri@sprintasia.co.id>
2023-12-02 14:27:23 +0700

    fetch and insert participants

---
+++ models/participants.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+func (Participants) TableName() string {
+	return "participants"
+}
+
+type Participants struct {
+	Id        uuid.UUID  `gorm:"column:id" json:"id"`
+	SessionID uuid.UUID  `gorm:"column:session_id" json:"session_id"`
+	UserID    uuid.UUID  `gorm:"column:user_id" json:"user_id"`
+	Status    bool       `gorm:"column:status" json:"status"`
+	CreatedAt time.Time  `gorm:"column:created_at" json:"created_at"`
+	CreatedBy uuid.UUID  `gorm:"column:created_by" json:"created_by"`
+	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updated_at"`
+	UpdatedBy uuid.UUID  `gorm:"column:updated_by" json:"updated_by"`
+	DeletedAt *time.Time `gorm:"column:deleted_at" json:"-"`
+	DeletedBy uuid.UUID  `gorm:"column:deleted_by" json:"deleted_by"`
+}
