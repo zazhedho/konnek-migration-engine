24af802500f948a0f1db62c400726c74bfa19082
Dimas Taufikurrisyad <dimas.taufikurrisyad@sprintasia.co.id>
2023-12-02 15:25:58 +0700

    Engine companies

--- companies.go
+++ companies.go
@@ -1,61 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
+	"konnek-migration/models"
 	"konnek-migration/utils"
-	"net/http"
 	"os"
 	"time"
 )
 
-type CompanyExist struct {
-	Id                   uuid.UUID  `json:"id" gorm:"column:id"`
-	Name                 string     `json:"name" gorm:"column:name"`
-	Status               bool       `json:"status" gorm:"column:status"`
-	CompanyCode          string     `json:"company_code" gorm:"column:company_code"`
-	LimitUser            int        `json:"limit_user" gorm:"column:limit_user"`
-	Email                string     `json:"email" gorm:"column:email"`
-	StartPeriod          time.Time  `json:"start_period" gorm:"column:start_period"`
-	EndPeriod            time.Time  `json:"end_period" gorm:"column:end_period"`
-	CreatedAt            time.Time  `json:"created_at" gorm:"column:created_at"`
-	UpdatedAt            time.Time  `json:"updated_at" gorm:"column:updated_at"`
-	DeletedAt            *time.Time `json:"-" gorm:"column:deleted_at"`
-	CorporateIdSandeza   string     `json:"corporate_id_sandeza" gorm:"column:corporate_id_sandeza"`
-	CorporateNameSandeza string     `json:"corporate_name_sandeza" gorm:"column:corporate_name_sandeza"`
-	DivisionIdSandeza    string     `json:"division_id_sandeza" gorm:"column:division_id_sandeza"`
-	DivisionNameSandeza  string     `json:"division_name_sandeza" gorm:"column:division_name_sandeza"`
-	UsernameCGW          string     `json:"username_cgw" gorm:"column:username_cgw"`
-	PasswordCGW          string     `json:"password_cgw" gorm:"column:password_cgw"`
-	WabaIdSandeza        string     `json:"waba_id_sandeza" gorm:"column:waba_id_sandeza"`
-	SenderIdSandeza      string     `json:"sender_id_sandeza" gorm:"column:sender_id_sandeza"`
-}
-
-type CompanyReeng struct {
-	Id           uuid.UUID  `json:"id" gorm:"column:id"`
-	Name         string     `json:"name" gorm:"column:name"`
-	Status       bool       `json:"status" gorm:"column:status"`
-	Code         string     `json:"code" gorm:"column:code"`
-	LimitUser    int        `json:"user_limit" gorm:"column:user_limit"`
-	Email        string     `json:"email" gorm:"column:email"`
-	StartPeriod  time.Time  `json:"start_period" gorm:"column:start_period"`
-	EndPeriod    time.Time  `json:"end_period" gorm:"column:end_period"`
-	Hosts        string     `json:"hosts" gorm:"column:hosts"`
-	CreatedAt    time.Time  `json:"created_at" gorm:"column:created_at"`
-	CreatedBy    uuid.UUID  `json:"created_by" gorm:"column:created_by"`
-	UpdatedAt    time.Time  `json:"updated_at" gorm:"column:updated_at"`
-	UpdatedBy    uuid.UUID  `json:"updated_by" gorm:"column:updated_by"`
-	DeletedAt    *time.Time `json:"-" gorm:"column:deleted_at"`
-	DeletedBy    uuid.UUID  `json:"deleted_by" gorm:"column:deleted_by"`
-	BoClientCode string     `json:"bo_client_code" gorm:"column:bo_client_code"`
-	BoDivCode    string     `json:"bo_div_code" gorm:"column:bo_div_code"`
-}
-
 func main() {
 	// Create source DB connection
-	scDB := utils.GetDBConnection(os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("USERNAME_DB"), os.Getenv("DATABASE_NAME"), os.Getenv("PASSWORD_DB"))
+	scDB := utils.GetDBConnection()
 	defer func(scDB *gorm.DB) {
 		err := scDB.Close()
 		if err != nil {
@@ -64,7 +22,7 @@ func main() {
 	}(scDB)
 
 	// Create destination DB connection
-	dstDB := utils.GetDBConnection(os.Getenv("RE_DATABASE_HOST"), os.Getenv("RE_DATABASE_PORT"), os.Getenv("RE_USERNAME_DB"), os.Getenv("RE_DATABASE_NAME"), os.Getenv("RE_PASSWORD_DB"))
+	dstDB := utils.GetDBNewConnection()
 	defer func(dstDB *gorm.DB) {
 		err := dstDB.Close()
 		if err != nil {
@@ -72,10 +30,100 @@ func main() {
 		}
 	}(dstDB)
 
-	if err := scDB.Order("created_at", order).Limit(perpage).Offset(offset).Find(&company).Error; err != nil {
-		if !gorm.IsRecordNotFoundError(err) {
-			sentry.CaptureException(err)
-		}
-		return []models.Company{}, http.StatusBadRequest, errors.New("failed to fetch company list" + err.Error())
+	logID := uuid.NewV4()
+	logPrefix := fmt.Sprintf("[%v] [Companies]", logID)
+	utils.WriteLog(fmt.Sprintf("%s start...", logPrefix), utils.LogLevelDebug)
+
+	tStart := time.Now()
+	debug := 0
+	debugT := time.Now()
+
+	var companiesSc []models.CompanyExist
+
+	if os.Getenv("COMPANYID") != "" {
+		scDB = scDB.Where("id = ?", os.Getenv("COMPANYID"))
 	}
+
+	//Fetch companies existing
+	if err := scDB.Find(&companiesSc).Error; err != nil {
+		utils.WriteLog(fmt.Sprintf("%s; fetch error: %v", logPrefix, err), utils.LogLevelError)
+		return
+	}
+
+	totalCompany := len(companiesSc)
+
+	debug++
+	utils.WriteLog(fmt.Sprintf("%s [FETCH] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, totalCompany, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+	debugT = time.Now()
+
+	insertedCount := 0
+	successCount := 0
+	errorCount := 0
+	var errorMessages []string
+
+	for _, company := range companiesSc {
+		var companyDst models.CompanyReeng
+
+		companyDst.Id = company.Id
+		companyDst.Code = company.CompanyCode
+		companyDst.Name = company.Name
+		companyDst.Email = company.Email
+		companyDst.LimitUser = company.LimitUser
+		companyDst.StartPeriod = company.StartPeriod
+		companyDst.EndPeriod = company.EndPeriod
+		companyDst.Status = company.Status
+		companyDst.CreatedAt = company.CreatedAt
+		companyDst.CreatedBy = uuid.Nil
+		companyDst.UpdatedAt = company.UpdatedAt
+		companyDst.UpdatedBy = uuid.Nil
+		companyDst.DeletedAt = company.DeletedAt
+		companyDst.DeletedBy = uuid.Nil
+
+		insertedCount++
+		reiInsertCount := 0
+	reInsert:
+		if err := dstDB.Create(&companyDst).Error; err != nil {
+			if errCode, ok := err.(*pq.Error); ok {
+				if errCode.Code == "23505" { //unique_violation
+					reiInsertCount++
+					companyDst.Id = uuid.NewV4()
+					if reiInsertCount < 3 {
+						goto reInsert
+					}
+				}
+			}
+			utils.WriteLog(fmt.Sprintf("%s; [INSERT] Error: %v", logPrefix, err), utils.LogLevelError)
+			errorCount++
+			errorMessages = append(errorMessages, fmt.Sprintf("%s [INSERT] Error: %v ; DATA: %v", time.Now(), err, companyDst))
+			continue
+		}
+
+		successCount++
+	}
+
+	// Write error messages to a text file
+	formattedTime := time.Now().Format("2006-01-02_150405")
+	errorFileLog := fmt.Sprintf("error_messages_companies_%s.log", formattedTime)
+	if len(errorMessages) > 0 {
+		createFile, errCreate := os.Create(errorFileLog)
+		if errCreate != nil {
+			utils.WriteLog(fmt.Sprintf("%s [ERROR] Create File Error Log; Error: %v;", logPrefix, errCreate), utils.LogLevelError)
+			return
+		}
+		defer createFile.Close()
+
+		for _, errMsg := range errorMessages {
+			_, errWrite := createFile.WriteString(errMsg + "\n")
+			if errWrite != nil {
+				utils.WriteLog(fmt.Sprintf("%s [ERROR] Write File Error Log; Filename: %s; Error: %v;", logPrefix, errorFileLog, errCreate), utils.LogLevelError)
+			}
+		}
+
+		utils.WriteLog(fmt.Sprintf("%s [ERROR] Error messages written to %s", logPrefix, errorFileLog), utils.LogLevelError)
+	}
+
+	debug++
+	utils.WriteLog(fmt.Sprintf("%s [INSERT] TOTAL_INSERTED: %d; TOTAL_SUCCESS: %d; TOTAL_ERROR: %v DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, insertedCount, successCount, errorCount, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+
+	utils.WriteLog(fmt.Sprintf("%s end; duration: %v", logPrefix, time.Now().Sub(tStart)), utils.LogLevelDebug)
 }
