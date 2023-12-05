6b7b81d4cd80dbe890e1637d6cd38a8e59a13695
nasrulqodri <nasrul.qodri@sprintasia.co.id>
2023-12-05 17:12:15 +0700

    handle error

--- models/role.go
+++ models/role.go
@@ -15,4 +15,5 @@ type Role struct {
 	CreatedAt time.Time  `json:"created_at" gorm:"column:created_at"`
 	UpdatedAt time.Time  `json:"updated_at" gorm:"column:updated_at"`
 	DeletedAt *time.Time `json:"-" gorm:"column:deleted_at"`
+	Error     string     `json:"error" gorm:"-"`
 }
