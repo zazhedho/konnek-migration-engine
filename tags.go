61f2382d68231ea90185afd3f45a2f4e039cfce1
Dimas Taufikurrisyad <dimas.taufikurrisyad@sprintasia.co.id>
2023-12-06 13:20:58 +0700

    Handle error

--- tags.go
+++ tags.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
+	"io/ioutil"
 	"konnek-migration/models"
 	"konnek-migration/utils"
 	"os"
@@ -30,7 +33,11 @@ func main() {
 	}(dstDB)
 
 	logID := uuid.NewV4()
-	logPrefix := fmt.Sprintf("[%v] [Tags]", logID)
+	appName := "tags"
+	if os.Getenv("APP_NAME") != "" {
+		appName = os.Getenv("APP_NAME")
+	}
+	logPrefix := fmt.Sprintf("[%v] [%s]", logID, appName)
 	utils.WriteLog(fmt.Sprintf("%s start...", logPrefix), utils.LogLevelDebug)
 
 	tStart := time.Now()
@@ -39,26 +46,56 @@ func main() {
 
 	var tagsSc []models.TagExist
 
-	if os.Getenv("COMPANYID") != "" {
-		scDB = scDB.Where("company_id = ?", os.Getenv("COMPANYID"))
+	if os.Getenv("GET_FROM_FILE") != "" {
+		utils.WriteLog(fmt.Sprintf("%s get from file %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelDebug)
+		// Read the JSON file
+		fileContent, err := ioutil.ReadFile("data/" + os.Getenv("GET_FROM_FILE"))
+		if err != nil {
+			fmt.Printf("%s Error reading file: %v\n", logPrefix, err)
+			utils.WriteLog(fmt.Sprintf("%s Error reading file: %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelError)
+			return
+		}
+
+		// Unmarshal the JSON data into the struct
+		err = json.Unmarshal(fileContent, &tagsSc)
+		if err != nil {
+			fmt.Printf("%s Error unmarshalling: %v\n", logPrefix, err)
+			utils.WriteLog(fmt.Sprintf("%s Error unmarshalling JSON: %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelError)
+			return
+		}
+		debug++
+		utils.WriteLog(fmt.Sprintf("%s [GET_FROM_FILE] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, len(tagsSc), debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+		debugT = time.Now()
+
+		err = os.Remove("data/" + os.Getenv("GET_FROM_FILE"))
+		if err != nil {
+			utils.WriteLog(fmt.Sprintf("%s Error Delete file: %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelError)
+		}
+	} else {
+		//Fetch from database
+
+		if os.Getenv("COMPANYID") != "" {
+			scDB = scDB.Where("company_id = ?", os.Getenv("COMPANYID"))
+		}
+
+		//Fetch companies existing
+		if err := scDB.Find(&tagsSc).Error; err != nil {
+			utils.WriteLog(fmt.Sprintf("%s; fetch error: %v", logPrefix, err), utils.LogLevelError)
+			return
+		}
+
+		totalTags := len(tagsSc)
+
+		debug++
+		utils.WriteLog(fmt.Sprintf("%s [FETCH] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, totalTags, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+		debugT = time.Now()
 	}
-
-	//Fetch companies existing
-	if err := scDB.Find(&tagsSc).Error; err != nil {
-		utils.WriteLog(fmt.Sprintf("%s; fetch error: %v", logPrefix, err), utils.LogLevelError)
-		return
-	}
-
-	totalTags := len(tagsSc)
-
-	debug++
-	utils.WriteLog(fmt.Sprintf("%s [FETCH] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, totalTags, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
-	debugT = time.Now()
 
 	insertedCount := 0
 	successCount := 0
 	errorCount := 0
-	var errorMessages []string
+	var errorMessages []models.TagExist
+	var errorDuplicates []models.TagExist
 
 	for _, tag := range tagsSc {
 		var tagsDst models.TagReeng
@@ -73,50 +110,36 @@ func main() {
 		tagsDst.DeletedAt = tag.DeletedAt
 
 		insertedCount++
-		//	reiInsertCount := 0
-		//reInsert:
+
 		if err := dstDB.Create(&tagsDst).Error; err != nil {
-			//if errCode, ok := err.(*pq.Error); ok {
-			//	if errCode.Code == "23505" { //unique_violation
-			//		reiInsertCount++
-			//		tagsDst.Id = uuid.NewV4()
-			//		if reiInsertCount < 3 {
-			//			goto reInsert
-			//		}
-			//	}
-			//}
 			utils.WriteLog(fmt.Sprintf("%s; [FAILED] [INSERT] Error: %v", logPrefix, err), utils.LogLevelError)
+
+			if errCode, ok := err.(*pq.Error); ok {
+				if errCode.Code == "23505" { //unique_violation
+					errorDuplicates = append(errorDuplicates, tag)
+					continue
+				}
+			}
 			errorCount++
-			errorMessages = append(errorMessages, fmt.Sprintf("%s [FAILED] [INSERT] Error: %v ; DATA: %v", time.Now(), err, tagsDst))
+			errorMessages = append(errorMessages, tag)
 			continue
 		}
 
 		successCount++
 	}
 
-	// Write error messages to a text file
-	formattedTime := time.Now().Format("2006-01-02_150405")
-	errorFileLog := fmt.Sprintf("error_messages_tags_%s.log", formattedTime)
-	if len(errorMessages) > 0 {
-		createFile, errCreate := os.Create(errorFileLog)
-		if errCreate != nil {
-			utils.WriteLog(fmt.Sprintf("%s [ERROR] Create File Error Log; Error: %v;", logPrefix, errCreate), utils.LogLevelError)
-			return
-		}
-		defer createFile.Close()
-
-		for _, errMsg := range errorMessages {
-			_, errWrite := createFile.WriteString(errMsg + "\n")
-			if errWrite != nil {
-				utils.WriteLog(fmt.Sprintf("%s [ERROR] Write File Error Log; Filename: %s; Error: %v;", logPrefix, errorFileLog, errCreate), utils.LogLevelError)
-			}
-		}
-
-		utils.WriteLog(fmt.Sprintf("%s [ERROR] Error messages written to %s", logPrefix, errorFileLog), utils.LogLevelError)
-	}
-
 	debug++
 	utils.WriteLog(fmt.Sprintf("%s [INSERT] TOTAL_INSERTED: %d; TOTAL_SUCCESS: %d; TOTAL_ERROR: %v DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, insertedCount, successCount, errorCount, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+
+	//write error to file
+	if len(errorMessages) > 0 {
+		filename := fmt.Sprintf("%s_%s", appName, time.Now().Format("2006_01_02"))
+		utils.WriteErrorMap(filename, errorMessages)
+	}
+	if len(errorDuplicates) > 0 {
+		filename := fmt.Sprintf("%s_%s_duplicate", appName, time.Now().Format("2006_01_02"))
+		utils.WriteErrorMap(filename, errorDuplicates)
+	}
 
 	utils.WriteLog(fmt.Sprintf("%s end; duration: %v", logPrefix, time.Now().Sub(tStart)), utils.LogLevelDebug)
 }
