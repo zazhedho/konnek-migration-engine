8d58b7b069b76d25a4854ea5d93f43622296903b
nasrulqodri <nasrul.qodri@sprintasia.co.id>
2023-12-08 13:56:20 +0700

    init report message

--- models/report_message.go
+++ models/report_message.go
@@ -36,3 +36,49 @@ type ReportMessage struct {
 	UpdatedAt  time.Time `json:"updated_at" gorm:"column:updated_at"`
 	UpdatedBy  string    `json:"updated_by" gorm:"column:updated_by"`
 }
+
+func (ChatMessageRpt) TableName() string {
+	return "chat_messages"
+}
+
+// ChatMessages struct db reengineering
+type ChatMessageRpt struct {
+	Id                uuid.UUID    `json:"id" gorm:"column:id"`
+	RoomId            uuid.UUID    `json:"room_id" gorm:"column:room_id"`
+	SessionId         uuid.UUID    `json:"session_id" gorm:"column:session_id"`
+	UserId            uuid.UUID    `json:"user_id" gorm:"column:user_id"`
+	Users             UserMessages `json:"user" gorm:"Foreignkey:UserId;association_foreignkey:Id;"`
+	MessageId         string       `json:"message_id" gorm:"column:message_id"`
+	ReplyId           string       `json:"reply_id" gorm:"column:reply_id"`
+	ProviderMessageId string       `json:"provider_message_id" gorm:"column:provider_message_id"`
+	FromType          string       `json:"from_type" gorm:"column:from_type"`
+	Type              string       `json:"type" gorm:"column:type"`
+	Text              string       `json:"text" gorm:"column:text"`
+	Payload           string       `json:"payload" gorm:"column:payload"`
+	Status            int          `json:"status" gorm:"column:status"`
+	MessageTime       *time.Time   `json:"message_time" gorm:"column:message_time"`
+	ReceivedAt        *time.Time   `json:"received_at" gorm:"column:received_at"`
+	ProcessedAt       *time.Time   `json:"processed_at" gorm:"column:processed_at"`
+	CreatedAt         time.Time    `json:"created_at" gorm:"column:created_at"`
+	CreatedBy         uuid.UUID    `json:"created_by" gorm:"column:created_by"`
+	UpdatedAt         time.Time    `json:"updated_at" gorm:"column:updated_at"`
+	UpdatedBy         uuid.UUID    `json:"updated_by" gorm:"column:updated_by"`
+	DeletedAt         *time.Time   `json:"-" gorm:"column:deleted_at"`
+	DeletedBy         uuid.UUID    `json:"deleted_by" gorm:"column:deleted_by"`
+	TextDeleted       string       `json:"-" gorm:"column:text_deleted"`
+	DeleteTime        *time.Time   `json:"-" gorm:"column:delete_time"`
+	Retry             int          `json:"retry_sending" gorm:"column:retry"`
+	RetryTime         *time.Time   `json:"retry_time" gorm:"column:retry_time"`
+	Error             string       `json:"error" gorm:"-"`
+}
+
+func (UserMessages) TableName() string {
+	return "users"
+}
+
+type UserMessages struct {
+	Id        uuid.UUID `json:"id" gorm:"column:id"`
+	Username  string    `json:"username" gorm:"column:username"`
+	Name      string    `json:"name" gorm:"column:name"`
+	CompanyId uuid.UUID `json:"company_id" gorm:"column:company_id"`
+}
---
+++ report_message.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/lib/pq"
+	uuid "github.com/satori/go.uuid"
+	"io/ioutil"
+	"konnek-migration/models"
+	"konnek-migration/utils"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	db := utils.GetDBNewConnection()
+	defer func(db *gorm.DB) {
+		err := db.Close()
+		if err != nil {
+			utils.WriteLog(fmt.Sprintf("Close Connection scDb; ERROR: %+v", err), utils.LogLevelError)
+		}
+	}(db)
+
+	dbReport := utils.GetDBReportConnection()
+	defer func(dbReport *gorm.DB) {
+		err := dbReport.Close()
+		if err != nil {
+			utils.WriteLog(fmt.Sprintf("Close Connection scDb; ERROR: %+v", err), utils.LogLevelError)
+		}
+	}(dbReport)
+
+	logID := uuid.NewV4()
+	appName := "report_message"
+	if os.Getenv("APP_NAME") != "" {
+		appName = os.Getenv("APP_NAME")
+	}
+
+	logPrefix := fmt.Sprintf("[%v] [%s]", logID, appName)
+	utils.WriteLog(fmt.Sprintf("%s start...", logPrefix), utils.LogLevelDebug)
+
+	tStart := time.Now()
+	debug := 0
+	debugT := time.Now()
+
+	var lists []models.ChatMessageRpt
+
+	// Get from file
+	if os.Getenv("GET_FROM_FILE") != "" {
+		utils.WriteLog(fmt.Sprintf("%s get from file %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelDebug)
+		// Read the JSON file
+		fileContent, err := ioutil.ReadFile("data/" + os.Getenv("GET_FROM_FILE"))
+		if err != nil {
+			fmt.Printf("%s Error reading file: %v\n", logPrefix, err)
+			utils.WriteLog(fmt.Sprintf("%s Error reading file: %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelError)
+			return
+		}
+
+		// Unmarshal the JSON data into the struct
+		err = json.Unmarshal(fileContent, &lists)
+		if err != nil {
+			fmt.Printf("%s Error unmarshalling: %v\n", logPrefix, err)
+			utils.WriteLog(fmt.Sprintf("%s Error unmarshalling JSON: %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelError)
+			return
+		}
+		debug++
+		utils.WriteLog(fmt.Sprintf("%s [GET_FROM_FILE] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, len(lists), debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+		debugT = time.Now()
+
+		err = os.Remove("data/" + os.Getenv("GET_FROM_FILE"))
+		if err != nil {
+			utils.WriteLog(fmt.Sprintf("%s Error Delete file: %s", logPrefix, os.Getenv("GET_FROM_FILE")), utils.LogLevelError)
+		}
+	} else {
+		//Fetch from database
+
+		//Set the filters
+		if os.Getenv("COMPANYID") != "" {
+			db = db.Preload("Users", func(db *gorm.DB) *gorm.DB {
+				return db.Where("company_id = ?", os.Getenv("COMPANYID"))
+			})
+		} else {
+			db = db.Preload("Users")
+		}
+
+		if os.Getenv("START_DATE") != "" && os.Getenv("END_DATE") != "" {
+			db = db.Where("created_at BETWEEN ? AND ?", os.Getenv("START_DATE"), os.Getenv("END_DATE"))
+		} else if os.Getenv("START_DATE") != "" && os.Getenv("END_DATE") == "" {
+			db = db.Where("created_at >=?", os.Getenv("START_DATE"))
+		} else if os.Getenv("START_DATE") == "" && os.Getenv("END_DATE") != "" {
+			db = db.Where("created_at <=?", os.Getenv("END_DATE"))
+		}
+
+		if os.Getenv("ORDER_BY") != "" {
+			sortMap := map[string]string{
+				"created_at": "created_at",
+			}
+			if strings.ToUpper(os.Getenv("ORDER_DIRECTION")) == "DESC" {
+				db = db.Order(sortMap[os.Getenv("ORDER_BY")] + " DESC")
+			} else {
+				db = db.Order(sortMap[os.Getenv("ORDER_BY")])
+			}
+		}
+
+		offset, _ := strconv.Atoi(os.Getenv("OFFSET"))
+		limit, _ := strconv.Atoi(os.Getenv("LIMIT"))
+
+		if offset > 0 {
+			db = db.Offset(offset)
+		}
+
+		if limit > 0 {
+			db = db.Limit(limit)
+		}
+
+		if err := db.Find(&lists).Error; err != nil {
+			utils.WriteLog(fmt.Sprintf("%s; fetch error: %v", logPrefix, err), utils.LogLevelError)
+			return
+		}
+		totalFetch := len(lists)
+
+		debug++
+		utils.WriteLog(fmt.Sprintf("%s [FETCH] TOTAL_FETCH: %d DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, totalFetch, debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+		debugT = time.Now()
+	}
+
+	//Insert into database report
+	var errorMessages []models.ChatMessageRpt
+	var errorDuplicates []models.ChatMessageRpt
+	totalInserted := 0
+
+	for _, list := range lists {
+		var m models.ReportMessage
+		m.TablePrefix = list.Users.CompanyId.String() + "_"
+		//check table, create table if it doesn't exist
+		//dbReport.AutoMigrate(&m)
+
+		m.Id = list.Id
+		m.MessageId = list.MessageId
+		m.ReplyId = list.ReplyId
+		m.RoomId = list.RoomId
+		m.SessionId = list.SessionId
+		m.FromType = list.FromType
+		m.UserId = list.UserId
+		m.Username = list.Users.Username
+		m.UserFullname = list.Users.Name
+		m.Type = list.Type
+		m.Text = list.Text
+		m.Payload = list.Payload
+		m.Status = list.Status
+		m.MessageTime = list.MessageTime
+		m.LastUpdate = time.Now()
+		m.CreatedAt = time.Now()
+		m.CreatedBy = "migration-engine"
+		m.UpdatedAt = time.Now()
+		m.UpdatedBy = "migration-engine"
+
+		if err := dbReport.Create(&m).Error; err != nil {
+			utils.WriteLog(fmt.Sprintf("%s; insert error: %v", logPrefix, err), utils.LogLevelError)
+			list.Error = err.Error()
+			if errCode, ok := err.(*pq.Error); ok {
+				if errCode.Code == "23505" { //unique_violation
+					errorDuplicates = append(errorDuplicates, list)
+					continue
+				}
+			}
+			errorMessages = append(errorMessages, list)
+			continue
+		}
+		totalInserted++
+	}
+	debug++
+	utils.WriteLog(fmt.Sprintf("%s [INSERT] TOTAL_INSERTED: %d; TOTAL_ERROR: %v DEBUG: %d; TIME: %s; TOTAL_TIME: %s;", logPrefix, totalInserted, len(errorMessages), debug, time.Now().Sub(debugT), time.Now().Sub(tStart)), utils.LogLevelDebug)
+
+	//write error to file
+	if len(errorMessages) > 0 {
+		filename := fmt.Sprintf("%s_%s_%v", appName, time.Now().Format("2006_01_02"), time.Now().Unix())
+		utils.WriteErrorMap(filename, errorMessages)
+	}
+	if len(errorDuplicates) > 0 {
+		filename := fmt.Sprintf("%s_%s_%v_duplicate", appName, time.Now().Format("2006_01_02"), time.Now().Unix())
+		utils.WriteErrorMap(filename, errorDuplicates)
+	}
+
+	utils.WriteLog(fmt.Sprintf("%s end; duration: %v", logPrefix, time.Now().Sub(tStart)), utils.LogLevelDebug)
+}
