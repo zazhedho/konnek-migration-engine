791509e4c50f12ff1a59ecd8502ee4213b7623ef
nasrulqodri <nasrul.qodri@sprintasia.co.id>
2023-12-12 18:26:04 +0700

    fixing filter by company_id

--- workers/users/users.go
+++ workers/users/users.go
@@ -81,7 +81,7 @@ func main() {
 		}
 	} else {
 		//Fetch from database
-
+		scDB = scDB.Unscoped()
 		//Set the filters
 		if os.Getenv("COMPANYID") != "" {
 			scDB = scDB.Where("company_id = ?", os.Getenv("COMPANYID"))
