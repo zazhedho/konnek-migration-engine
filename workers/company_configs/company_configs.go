7ab6126c482b6e8ee3aba0e1f2c6046669330e83
Dimas Taufikurrisyad <dimas.taufikurrisyad@sprintasia.co.id>
2023-12-18 15:57:17 +0700

    Enhance fetch data company config where "company_id in"

--- workers/company_configs/company_configs.go
+++ workers/company_configs/company_configs.go
@@ -11,6 +11,7 @@ import (
 	"konnek-migration/sdk"
 	"konnek-migration/utils"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -78,7 +79,8 @@ func main() {
 		//Fetch from database
 		scDB = scDB.Unscoped()
 		if os.Getenv("COMPANYID") != "" {
-			scDB = scDB.Where("company_id = ? ", os.Getenv("COMPANYID"))
+			companyId := strings.Split(os.Getenv("COMPANYID"), ",")
+			scDB = scDB.Where("id IN (?)", companyId)
 		}
 
 		//Fetch companies existing
