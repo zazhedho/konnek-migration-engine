0ca27c1fbf4535d11bb21759117cd560f5fb2d65
Dimas Taufikurrisyad <dimas.taufikurrisyad@sprintasia.co.id>
2023-12-19 19:12:15 +0700

    Enhance engine tags where use 'company_id in'

--- workers/tags/tags.go
+++ workers/tags/tags.go
@@ -10,6 +10,7 @@ import (
 	"konnek-migration/models"
 	"konnek-migration/utils"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -77,7 +78,8 @@ func main() {
 		//Fetch from database
 
 		if os.Getenv("COMPANYID") != "" {
-			scDB = scDB.Where("company_id = ?", os.Getenv("COMPANYID"))
+			companyId := strings.Split(os.Getenv("COMPANYID"), ",")
+			scDB = scDB.Where("company_id IN (?)", companyId)
 		}
 
 		//Fetch companies existing
